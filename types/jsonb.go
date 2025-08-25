eba82af25060cbf38678065cb00c0eaf23ba70ee
Nawa Aripurna <nap.aripurna@gmail.com>
2025-08-25 10:47:02 +0800

    add jsonb type

---
+++ types/jsonb.go
@@ -0,0 +1,45 @@
+package types
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+)
+
+type JSONB map[string]any
+
+// Scan implements sql.Scanner.
+func (j *JSONB) Scan(src any) error {
+	var result JSONB
+
+	if data, ok := src.([]byte); ok {
+		if err := json.Unmarshal(data, &result); err != nil {
+			return err
+		}
+	}
+
+	if data, ok := src.(string); ok {
+		if err := json.Unmarshal([]byte(data), &result); err != nil {
+			return err
+		}
+	}
+
+	j = &result
+
+	return nil
+}
+
+// Value implements driver.Valuer.
+func (j JSONB) Value() (driver.Value, error) {
+	if len(j) == 0 {
+		return []byte(nil), nil
+	}
+
+	res, err := json.Marshal(j)
+
+	return res, err
+}
+
+func NewValuer() sql.Scanner {
+	return &JSONB{}
+}
---
+++ types/jsonb_test.go
@@ -0,0 +1,35 @@
+package types_test
+
+import (
+	"encoding/json"
+
+	"github.com/Nuanu-com/go-utils/types"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("JSONB", func() {
+	Describe("MarshalJSON", func() {
+		It("returns the bytes", func() {
+			data := types.JSONB{
+				"name": "George",
+			}
+
+			res, err := json.Marshal(data)
+
+			Expect(err).To(BeNil())
+			Expect(res).To(Equal([]byte(`{"name":"George"}`)))
+		})
+	})
+
+	Describe("UnmarshalJSON", func() {
+		It("converts json string into JSONB object", func() {
+			var obj types.JSONB
+
+			err := json.Unmarshal([]byte(`{"name":"George"}`), &obj)
+
+			Expect(err).To(BeNil())
+			Expect(obj).To(Equal(types.JSONB{"name": "George"}))
+		})
+	})
+})

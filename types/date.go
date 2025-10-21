858a1306ea77482c1d455aa35d76e4183da6b351
Nawa Aripurna <nap.aripurna@gmail.com>
2025-10-21 17:20:04 +0800

    add Valuer and Scanner interface for Date

--- types/date.go
+++ types/date.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"strings"
 	"time"
@@ -11,6 +12,24 @@ type Date struct {
 }
 
 const StandardDateFormat = "2006-01-02"
+
+func (a Date) Value() (driver.Value, error) {
+	return a.Time.Format(StandardDateFormat), nil
+}
+
+func (a *Date) Scan(value interface{}) error {
+	if value == nil {
+
+		return nil
+	}
+
+	if dateTime, ok := value.(time.Time); ok {
+		a.Time = time.Date(dateTime.Year(), dateTime.Month(), dateTime.Day(), 0, 0, 0, 0, time.UTC)
+		return nil
+	}
+
+	return nil
+}
 
 // UnmarshalJSON implements json.Unmarshaler.
 func (d *Date) UnmarshalJSON(data []byte) error {
